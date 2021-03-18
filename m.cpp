dd9e6f3d5b8c7bc9b3be7a4d3999dab20a031e2f
Aryeh Frankel <aryeh.frankel@verizonmedia.com>
2021-03-17 21:30:06 -0400

    a

--- m.cpp
+++ m.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-void addTo(std::string* toChange, const char* source) {
-    toChange->append(source, strlen(source));
+void die(const char* msg) {
+    cout << msg;
+    exit(1);
 }
+
+class T{
+    private:
+        int i;
+    public:
+        T() { std::cout << "making a T!"; }
+        int getI() { return i; }
+        void setI(int i) { this->i = i; }
+};
+
 int main(int argc, char** argv) {
-    return 0;
+    // C way of dymamic allocation
+    T *a = (T *) malloc(sizeof(T));
+    a->setI(5);
+    std::cout << a->getI() << std::endl;
+    free(a);
+
+    // C++ way 
+    T *b = new T;
+    b->setI(10);
+    std::cout << b->getI() << std::endl;
+    free(b);
 }
