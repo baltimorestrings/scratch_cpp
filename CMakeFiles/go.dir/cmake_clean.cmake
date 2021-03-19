9f8db0b1ad07426c2f5082dc54780633c60ed3f3
Aryeh Frankel <aryeh.frankel@verizonmedia.com>
2021-03-19 02:01:00 -0400

    a

---
+++ CMakeFiles/go.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/go.dir/main.cpp.o"
+  "go"
+  "go.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/go.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
